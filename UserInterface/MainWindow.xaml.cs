37bb2bdd936a4fd88280747b169edb24e1e93fcc
stefanocastello <steisbetter@me.com>
2017-12-19 13:09:55 +0100

    GUI add selection

--- UserInterface/MainWindow.xaml.cs
+++ UserInterface/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using BugReportFactory;
 using Database;
 
@@ -23,23 +13,25 @@ namespace UserInterface
     public partial class MainWindow : Window
     {
         private BugReportContext Db;
+
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
 
         private void ButtonReset_Click1(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("|"+NewPwdField.Text+"|");
+            //MessageBox.Show("|"+NewPwdField.Text+"|");
             Factory.InitializeBugTracking(null, NewPwdField.Text);
+            NewPwdField.Clear();
         }
 
         private void ButtonConnect_Click1(object sender, RoutedEventArgs e)
         {
-            
-            MessageBox.Show(Adminpwd.Password);
-            Db = Factory.LoadBugTracking(null,Adminpwd.Password);
+            Db = Factory.LoadBugTracking(null, Adminpwd.Password);
+            DataBox.Document.Blocks.Clear();
+            DataBox.AppendText("Connected !");
             UsrTable.Visibility = 0;
             Products.Visibility = 0;
 
@@ -68,26 +60,27 @@ namespace UserInterface
             //db.Users.Add(user2);
             //db.SaveChanges();
 
-            var query = from u in Db.Users
-                        select new{ u.Name, u.Surname, u.Dob, u.CodFisc, u.Indirizzo};
-            foreach (var q in query)
-                DataBox.AppendText(q.Name + "\t" + q.Surname + "\t" + q.CodFisc + "\t" + q.Dob + "\t" + q.Indirizzo);
 
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            
+            DataBox.Document.Blocks.Clear();
+            var query = from u in Db.Users
+                select new {u.Name, u.Surname, u.Dob, u.CodFisc, u.Indirizzo};
+            foreach (var q in query)
+                DataBox.AppendText(q.Name + "\t" + q.Surname + "\t" + q.CodFisc + "\t" + q.Dob + "\t" +
+                                   q.Indirizzo.Via + "\t" + q.Indirizzo.Civico + "\t" + q.Indirizzo.Interno);
+
+        }
+
+        private void RadioButton_Checked2(object sender, RoutedEventArgs e)
+        {
+            DataBox.Document.Blocks.Clear();
+            var query = from p in Db.Products
+                select new {p.CommName, p.Id};
+            foreach (var q in query)
+                DataBox.AppendText(q.CommName + "\t" + q.Id);
         }
     }
 }
